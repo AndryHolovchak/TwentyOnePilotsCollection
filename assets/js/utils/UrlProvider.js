56a70ad2cb3f1eeb4f9867c07ae69140348c7ab1
Andry Holovchak <andryholovchak@gmail.com>
2020-09-25 16:50:43 +0300

    A gew more paths changed to relative

--- assets/js/utils/UrlProvider.js
+++ assets/js/utils/UrlProvider.js
@@ -1,12 +1,12 @@
 class UrlProvider {
   static getUrlToMp3(songId) {
-    return `/data/mp3/${songId}.mp3`;
+    return `../../../data/mp3/${songId}.mp3`;
   }
   static getUrlToAlbumCover(albumId, size) {
-    return `/data/covers/${size}/${albumId}.jpg`;
+    return `../../../data/covers/${size}/${albumId}.jpg`;
   }
   static getUrlToAlbumJson(albumId) {
-    return `/data/albums/${albumId}.json`;
+    return `../../../data/albums/${albumId}.json`;
   }
   static getUrlToYoutubeIframe(videoId) {
     return `https://www.youtube.com/embed/${videoId}`;
